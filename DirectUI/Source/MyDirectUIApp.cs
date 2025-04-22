997b5d9e1a2ae3b398407bc76e29e3904236858a
Parsa <Parsa@DESKTOP-32S8QLS>
2025-04-22 20:45:38 +0330

    Revert "Failed"
    This reverts commit 24a0c6d2803973b0929976606303eb68229c33c4.

--- DirectUI/Source/MyDirectUIApp.cs
+++ DirectUI/Source/MyDirectUIApp.cs
@@ -25,7 +25,7 @@ public class MyDirectUIApp : Direct2DAppWindow
         // 1. Create the pack with global settings
         buttonTheme = new ButtonStylePack()
         {
-            Roundness = 1f,
+            Roundness = 0f,
             BorderLength = 1.0f, // Sets all borders to 1.0f for ALL states initially
             FontSize = 14f,
         };
