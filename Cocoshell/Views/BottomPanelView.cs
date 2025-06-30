47c350e48e8f3cd70fc437aa475eeadbc97bcde0
Parsa <pkafsharix@gmail.com>
2025-06-30 12:42:29 +0330

    Hid absolute path from inspector

--- Cocoshell/Views/BottomPanelView.cs
+++ Cocoshell/Views/BottomPanelView.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Numerics;
+﻿using System.Numerics;
 using Vortice.Mathematics;
 
 namespace DirectUI;
